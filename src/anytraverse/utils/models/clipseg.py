8929a03774c9e1915d7a134117ae1527a5ae0651
Sattwik Sahu <sattwik21@iiserb.ac.in>
2025-12-08 11:53:55 +0530

    fix[pipeline]: output shape bug

--- src/anytraverse/utils/models/clipseg.py
+++ src/anytraverse/utils/models/clipseg.py
@@ -65,9 +65,7 @@ class CLIPSegAttentionMapping[TImage: anyt.Image](PromptAttentionMapping[TImage]
                         return_tensors="pt",
                     ).to(device=self._device)
                     # Perform inference
-                    output = torch.sigmoid(
-                        resize(self._model(**inputs).logits.squeeze(0))
-                    )
+                    output = torch.sigmoid(resize(self._model(**inputs).logits))
                     # Add the prompt attention map to the list
                     maps.append(output)
             else:
